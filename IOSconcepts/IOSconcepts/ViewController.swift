becd0d92c8ec16e0d2bc7f93f1280cb08fb21f8e
cvarama <sivaramakrishna.a@gmail.com>
2017-07-30 23:22:29 +0530

    Initial Commit

---
+++ IOSconcepts/IOSconcepts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSconcepts
+//
+//  Created by Sivarama Krishna on 30/07/17.
+//  Copyright © 2017 Sivarama Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
